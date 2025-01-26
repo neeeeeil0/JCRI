368b2832838c57dd54bda48bbe9e2e9bb69ad6ac
jhypsss <jhonpatrick.torres@tup.edu.ph>
2025-01-27 01:37:29 +0800

    Updated Job Vacancy

--- admin/vacancy/add.php
+++ admin/vacancy/add.php
@@ -5,177 +5,158 @@
      }
 
 ?>
- <form class="form-horizontal span6" action="controller.php?action=add" method="POST">
+<form class="form-horizontal span6" action="controller.php?action=add" method="POST">
 
-                <div class="row">
-                   <div class="col-lg-12">
-                      <h1 class="page-header">Add New Job Vacancy</h1>
-                    </div>
-                    <!-- /.col-lg-12 -->
-                 </div> 
+<div class="row">
+    <div class="col-lg-12">
+      <h1 class="page-header">Add New Job Vacancy</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+  </div> 
 
-                 <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "COMPANYNAME">Company Name:</label>
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "COMPANYNAME">Company Name:</label>
 
-                      <div class="col-md-8">
-                        <select class="form-control input-sm" id="COMPANYID" name="COMPANYID" required>
-                          <option value="">Select</option>
-                          <?php 
-                            $sql ="Select * From tblcompany order by COMPANYNAME";
-                            $mydb->setQuery($sql);
-                            $res  = $mydb->loadResultList();
-                            foreach ($res as $row) {
-                              # code...
-                              echo '<option value='.$row->COMPANYID.'>'.$row->COMPANYNAME.'</option>';
-                            }
+      <div class="col-md-8">
+        <select class="form-control input-sm" id="COMPANYID" name="COMPANYID" required>
+          <option value="">Select</option>
+          <?php 
+            $sql ="Select * From tblcompany order by COMPANYNAME";
+            $mydb->setQuery($sql);
+            $res  = $mydb->loadResultList();
+            foreach ($res as $row) {
+              # code...
+              echo '<option value='.$row->COMPANYID.'>'.$row->COMPANYNAME.'</option>';
+            }
+          ?>
+        </select>
+      </div>
+    </div>
+  </div>   
 
-                          ?>
-                        </select>
-                      </div>
-                    </div>
-                  </div>  
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "CATEGORY">Classification:</label>
 
-                     
-                  <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "OCCUPATIONTITLE">Job Title:</label> 
-                      <div class="col-md-8">
-                         <input class="form-control input-sm" id="OCCUPATIONTITLE" name="OCCUPATIONTITLE" placeholder="Occupation Title"   autocomplete="none"/> 
-                      </div>
-                    </div>
-                  </div> 
+      <div class="col-md-8">
+        <select class="form-control input-sm" id="CATEGORY" name="CATEGORY" required>
+          <option value="">Select</option>
+          <?php 
+            $sql ="Select * From tblcategory ORDER BY CATEGORY";
+            $mydb->setQuery($sql);
+            $res  = $mydb->loadResultList();
+            foreach ($res as $row) {
+              # code...
+              echo '<option value='.$row->CATEGORYID.'>'.$row->CATEGORY.'</option>';
+            }
 
-                  <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "CATEGORY">Classification:</label>
+          ?>
+        </select>
+      </div>
+    </div>
+  </div> 
 
-                      <div class="col-md-8">
-                        <select class="form-control input-sm" id="CATEGORY" name="CATEGORY" required>
-                          <option value="">Select</option>
-                          <?php 
-                            $sql ="Select * From tblcategory ORDER BY CATEGORY";
-                            $mydb->setQuery($sql);
-                            $res  = $mydb->loadResultList();
-                            foreach ($res as $row) {
-                              # code...
-                              echo '<option value='.$row->CATEGORYID.'>'.$row->CATEGORY.'</option>';
-                            }
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "OCCUPATIONTITLE">Job Title:</label> 
+      <div class="col-md-8">
+          <input class="form-control input-sm" id="OCCUPATIONTITLE" name="OCCUPATIONTITLE" placeholder="Job Title"   autocomplete="off"/> 
+      </div>
+    </div>
+  </div>
 
-                          ?>
-                        </select>
-                      </div>
-                    </div>
-                  </div>  
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "SALARIES">Salary:</label> 
+      <div class="col-md-8">
+          <input class="form-control input-sm" id="SALARIES" name="SALARIES" placeholder="Salary"   autocomplete="off"/> 
+      </div>
+    </div>
+  </div>  
 
-                  <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "SALARIES">Salary:</label> 
-                      <div class="col-md-8">
-                         <input class="form-control input-sm" id="SALARIES" name="SALARIES" placeholder="Salary"   autocomplete="none"/> 
-                      </div>
-                    </div>
-                  </div>  
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "JOBTYPE">Job Type:</label> 
+      <div class="col-md-8">
+        <select class="form-control input-sm" id="JOBTYPE" name="JOBTYPE">
+          <option value="">Select</option>
+          <option value="On-Site">On-Site</option>
+          <option value="Work from Home">Work from Home</option>
+          <option value="Hybrid">Hybrid</option>
+        </select>
+      </div>
+    </div>
+  </div>
 
-                  <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "JOBTYPE">Job Type:</label> 
-                      <div class="col-md-8">
-                          <select class="form-control input-sm" id="JOBTYPE" name="JOBTYPE">
-                          <option value="None">Select</option>
-                           <option>Hybrid</option>
-                           <option>Work from Home</option>
-                           <option>On-Site</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div> 
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "JOBDESCRIPTION">Job Description:</label> 
+      <div class="col-md-8">
+        <textarea class="form-control input-sm" id="JOBDESCRIPTION" name="JOBDESCRIPTION" placeholder="Job Description"   autocomplete="none"></textarea> 
+      </div>
+    </div>
+  </div>  
 
-                  <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "JOBDESCRIPTION">Job Description:</label> 
-                      <div class="col-md-8">
-                        <textarea class="form-control input-sm" id="JOBDESCRIPTION" name="JOBDESCRIPTION" placeholder="Job Description"   autocomplete="none"></textarea> 
-                      </div>
-                    </div>
-                  </div>  
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "QUALIFICATION_WORKEXPERIENCE">Qualification/Work Experience:</label> 
+      <div class="col-md-8">
+        <textarea class="form-control input-sm" id="QUALIFICATION_WORKEXPERIENCE" name="QUALIFICATION_WORKEXPERIENCE" placeholder="Qualification/Experience"   autocomplete="none"></textarea> 
+      </div>
+    </div>
+  </div>
 
-                 <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "PREFEREDSEX">Prefered Sex:</label> 
-                      <div class="col-md-8">
-                          <select class="form-control input-sm" id="PREFEREDSEX" name="PREFEREDSEX">
-                          <option value="None">Select</option>
-                           <option>Male</option>
-                           <option>Female</option>
-                           <option>Male/Female</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div> 
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "PREFEREDSEX">Prefered Sex:</label> 
+      <div class="col-md-8">
+          <select class="form-control input-sm" id="PREFEREDSEX" name="PREFEREDSEX">
+          <option value="None">Select</option>
+            <option>Male</option>
+            <option>Female</option>
+            <option>Male/Female</option>
+        </select>
+      </div>
+    </div>
+  </div>
 
-                  <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "EDUCATION">Educational Attainment:</label> 
-                      <div class="col-md-8">
-                          <select class="form-control input-sm" id="EDUCATION" name="EDUCATION">
-                          <option value="None">Select</option>
-                           <option>Doctorate</option>
-                           <option>Masteral</option>
-                           <option>College</option>
-                           <option>Senior High School</option>
-                           <option>High School</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div> 
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "JOBSTATUS">Status:</label> 
+      <div class="col-md-8">
+          <select class="form-control input-sm" id="JOBSTATUS" name="JOBSTATUS">
+          <option value="Closed">Select</option>
+            <option>Open</option>
+            <option>Closed</option>
+        </select>
+      </div>
+    </div>
+  </div>  
 
-                  <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "QUALIFICATION_WORKEXPERIENCE">Qualification/Experience:</label> 
-                      <div class="col-md-8">
-                        <textarea class="form-control input-sm" id="QUALIFICATION_WORKEXPERIENCE" name="QUALIFICATION_WORKEXPERIENCE" placeholder="Qualification/Experience"   autocomplete="none"></textarea> 
-                      </div>
-                    </div>
-                  </div> 
+  <div class="form-group">
+    <div class="col-md-8">
+      <label class="col-md-4 control-label" for=
+      "idno"></label>  
 
-                  <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "JOBSTATUS">Status:</label> 
-                      <div class="col-md-8">
-                          <select class="form-control input-sm" id="JOBSTATUS" name="JOBSTATUS">
-                          <option value="Closed">Select</option>
-                           <option>Open</option>
-                           <option>Closed</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div>  
+      <div class="col-md-8">
+          <button class="btn btn-primary btn-sm" name="save" type="submit" ><span class="fa fa-save fw-fa"></span> Save</button>
+      <!-- <a href="index.php" class="btn btn-info"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;<strong>Back</strong></a> -->
+      
+      </div>
+    </div>
+  </div> 
 
-                  <div class="form-group">
-                    <div class="col-md-8">
-                      <label class="col-md-4 control-label" for=
-                      "idno"></label>  
-
-                      <div class="col-md-8">
-                         <button class="btn btn-primary btn-sm" name="save" type="submit" ><span class="fa fa-save fw-fa"></span> Save</button>
-                      <!-- <a href="index.php" class="btn btn-info"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;<strong>Back</strong></a> -->
-                     
-                     </div>
-                    </div>
-                  </div> 
- 
-          
-        </form>
+  
+</form>
       
  
--- admin/vacancy/controller.php
+++ admin/vacancy/controller.php
@@ -43,11 +43,12 @@ switch ($action) {
 				$job->OCCUPATIONTITLE					= $_POST['OCCUPATIONTITLE'];
 				$job->REQ_NO_EMPLOYEES					= $_POST['REQ_NO_EMPLOYEES'];
 				$job->SALARIES							= $_POST['SALARIES'];
-				$job->DURATION_EMPLOYEMENT				= $_POST['DURATION_EMPLOYEMENT'];
+				$job->JOBTYPE							= $_POST['JOBTYPE'];
+				//$job->DURATION_EMPLOYEMENT				= $_POST['DURATION_EMPLOYEMENT'];
 				$job->QUALIFICATION_WORKEXPERIENCE		= $_POST['QUALIFICATION_WORKEXPERIENCE'];
 				$job->JOBDESCRIPTION					= $_POST['JOBDESCRIPTION'];
 				$job->PREFEREDSEX						= $_POST['PREFEREDSEX'];
-				$job->SECTOR_VACANCY					= $_POST['SECTOR_VACANCY']; 
+				//$job->SECTOR_VACANCY					= $_POST['SECTOR_VACANCY']; 
 				$job->DATEPOSTED						= date('Y-m-d H:i:s');
 				$job->PUBLISHERID 						= $publisher;
 				$job->JOBSTATUS							= $_POST['JOBSTATUS'];
@@ -91,11 +92,11 @@ switch ($action) {
 				$job->OCCUPATIONTITLE					= $_POST['OCCUPATIONTITLE'];
 				$job->REQ_NO_EMPLOYEES					= $_POST['REQ_NO_EMPLOYEES'];
 				$job->SALARIES							= $_POST['SALARIES'];
-				$job->DURATION_EMPLOYEMENT				= $_POST['DURATION_EMPLOYEMENT'];
+				//$job->DURATION_EMPLOYEMENT				= $_POST['DURATION_EMPLOYEMENT'];
 				$job->QUALIFICATION_WORKEXPERIENCE		= $_POST['QUALIFICATION_WORKEXPERIENCE'];
 				$job->JOBDESCRIPTION					= $_POST['JOBDESCRIPTION'];
 				$job->PREFEREDSEX						= $_POST['PREFEREDSEX'];
-				$job->SECTOR_VACANCY					= $_POST['SECTOR_VACANCY'];
+				//$job->SECTOR_VACANCY					= $_POST['SECTOR_VACANCY'];
 				$job->JOBSTATUS							= $_POST['JOBSTATUS'];
 				$job->DATEUPDATED						= date('Y-m-d H:i:s');
 				$job->update($_POST['JOBID']);
