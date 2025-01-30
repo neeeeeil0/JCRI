2e8d27a28c94bf6032b9bcda59aa373f1e19719a
jhypsss <jhonpatrick.torres@tup.edu.ph>
2025-01-31 07:43:13 +0800

    Dashboard route

---
+++ admin/dashboard/index.php
@@ -0,0 +1,32 @@
+<?php
+require_once("../../include/initialize.php");
+//checkAdmin();
+  	 if (!isset($_SESSION['ADMIN_USERID'])){
+      redirect(web_root."admin/index.php");
+     }
+
+$view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
+$header=$view;
+$title="Dashboard";
+switch ($view) {
+	case 'list' :
+		$content    = 'list.php';		
+		break;
+
+	case 'add' :
+		$content    = 'add.php';		
+		break;
+
+	case 'edit' :
+		$content    = 'edit.php';		
+		break;
+    case 'view' :
+		$content    = 'view.php';		
+		break;
+
+	default :
+		$content    = 'home.php';		
+}
+require_once ("../theme/admin.php");
+?>
+  
